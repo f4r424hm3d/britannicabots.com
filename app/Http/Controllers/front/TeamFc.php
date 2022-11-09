476017d7e2c202ff8c7a5d85786441c692200152
MohdFaraz <farazahmad280@gmail.com>
2022-11-09 18:25:38 +0530

    Team Front Done

--- app/Http/Controllers/front/TeamFc.php
+++ app/Http/Controllers/front/TeamFc.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Team;
 use Illuminate\Http\Request;
 
 class TeamFc extends Controller
 {
   public function index(Request $request)
   {
-    return view('front.team');
+    $rows = Team::where(['status' => 1])->get();
+    $data = compact('rows');
+    return view('front.team')->with($data);
   }
 }
--- resources/views/front/team.blade.php
+++ resources/views/front/team.blade.php
@@ -1,6 +1,6 @@
 @extends('front.layouts.main')
 @push('title')
-<title>About</title>
+<title>Team</title>
 @endpush
 @section('main-section')
 <!--body content wrap start-->
@@ -25,96 +25,26 @@
     <div class="container">
       <div class="row d-flex align-items-center">
 
-        <div class="col-md-4 col-lg-4">
-          <div class="staff-member mb-4">
-            <div class="pl-4 pr-4 pt-4 text-center">
-              <div class="custom-port-col-image w-anuimation-image4"><img alt=""
-                  src="{{ url('/front/') }}/img/team-2.jpg" class="img-fluid m-auto"></div>
-              <div class="card-body">
-                <h5 class="teacher mb-0">Aman Ahlawat</h5>
-                <span>CEO & FOUNDER</span>
-              </div>
-            </div>
-            <div class="overlay d-flex align-items-center justify-content-center z-index">
-              <div class="overlay-inner">
-                <p class="lead">"Dramatically leverage existing fully researched platforms vis-a-vis viral." </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        @foreach ($rows as $row)
         <div class="col-md-4 col-lg-4">
           <div class="staff-member mb-4 shadow">
             <div class="card pl-4 pr-4 pt-4 text-center">
-              <div class="custom-port-col-image w-anuimation-image4"><img alt=""
-                  src="{{ url('/front/') }}/img/team-1.jpg" class="img-fluid m-auto"></div>
+              <div class="custom-port-col-image w-anuimation-image4">
+                <img alt="{{ $row->name }}" src="{{ asset($row->profile_picture_path) }}" class="img-fluid m-auto">
+              </div>
               <div class="card-body">
-                <h5 class="teacher mb-0">Mohd. Rafey</h5>
-                <span>Designation type here</span>
+                <h5 class="teacher mb-0">{{ $row->name }}</h5>
+                <span>{{ $row->designation }}</span>
               </div>
             </div>
             <div class="overlay d-flex align-items-center justify-content-center z-index">
               <div class="overlay-inner">
-                <p class="lead">"Dramatically leverage existing fully researched platforms vis-a-vis viral." </p>
+                <p class="lead">"{{ $row->quote }}" </p>
               </div>
             </div>
           </div>
         </div>
-
-        <div class="col-md-4 col-lg-4">
-          <div class="staff-member mb-4 shadow">
-            <div class="card pl-4 pr-4 pt-4 text-center">
-              <div class="custom-port-col-image w-anuimation-image4"><img alt=""
-                  src="{{ url('/front/') }}/img/team-1.jpg" class="img-fluid m-auto"></div>
-              <div class="card-body">
-                <h5 class="teacher mb-0">Mohd. Faraz</h5>
-                <span>Designation type here</span>
-              </div>
-            </div>
-            <div class="overlay d-flex align-items-center justify-content-center z-index">
-              <div class="overlay-inner">
-                <p class="lead">"Dramatically leverage existing fully researched platforms vis-a-vis viral." </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 col-lg-4">
-          <div class="staff-member mb-4 shadow">
-            <div class="card pl-4 pr-4 pt-4 text-center">
-              <div class="custom-port-col-image w-anuimation-image4"><img alt=""
-                  src="{{ url('/front/') }}/img/team-1.jpg" class="img-fluid m-auto"></div>
-              <div class="card-body">
-                <h5 class="teacher mb-0">Saurabh</h5>
-                <span>Designation type here</span>
-              </div>
-            </div>
-            <div class="overlay d-flex align-items-center justify-content-center z-index">
-              <div class="overlay-inner">
-                <p class="lead">"Dramatically leverage existing fully researched platforms vis-a-vis viral." </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 col-lg-4">
-          <div class="staff-member mb-4 shadow">
-            <div class="card pl-4 pr-4 pt-4 text-center">
-              <div class="custom-port-col-image w-anuimation-image4"><img alt=""
-                  src="{{ url('/front/') }}/img/team-1.jpg" class="img-fluid m-auto"></div>
-              <div class="card-body">
-                <h5 class="teacher mb-0">Shahbuddin Aamir</h5>
-                <span>Designation type here</span>
-              </div>
-            </div>
-            <div class="overlay d-flex align-items-center justify-content-center z-index">
-              <div class="overlay-inner">
-                <p class="lead">"Dramatically leverage existing fully researched platforms vis-a-vis viral." </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        @endforeach
       </div>
     </div>
   </section>
