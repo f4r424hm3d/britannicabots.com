5a8e59171205de65d2b6f814925f6b96ed6940e0
MohdFaraz <farazahmad280@gmail.com>
2025-07-23 12:17:17 +0530

    Update HeaderServiceProvider

--- app/Providers/HeaderServiceProvider.php
+++ app/Providers/HeaderServiceProvider.php
@@ -2,12 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Country;
-use App\Models\CourseCategory;
-use App\Models\Exam;
 use App\Models\HireResourcesCategory;
-use App\Models\Level;
-use App\Models\SitePage;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
